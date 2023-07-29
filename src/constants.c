3ec9c07b7a70994270db73f612f376d1ae7a50bd
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-07-29 18:01:12 +0300

    Add Color constants

---
+++ src/constants.c
@@ -0,0 +1,4 @@
+#include "constants.h"
+
+const SDL_Color TITLE_COLOR = {156, 14, 38, 255};
+const SDL_Color HEADER_COLOR = {156, 14, 38, 255};
--- src/constants.h
+++ src/constants.h
@@ -1,10 +1,17 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <SDL.h>
+
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+#define MENU_ITM_HRZTNL_SPCING 40
+#define START_SCREEN_MENU_ITEM_COUNT 2
 
 #define WINDOW_W_HALF (WINDOW_WIDTH / 2)
 #define WINDOW_H_HALF (WINDOW_HEIGHT / 2)
+
+extern const SDL_Color TITLE_COLOR;
+extern const SDL_Color HEADER_COLOR;
 
 #endif
